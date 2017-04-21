364f7b11dea2c1b562cfc7b4960d7d92d5e9a8dc
Jean Cochrane <jean@jeancochrane.com>
2017-04-21 10:49:00 -0500

    Debug manylinux-cython

--- .manylinux-cython.sh
+++ .manylinux-cython.sh
@@ -2,5 +2,7 @@
 # Passes the right version of cython into update_cpp.sh for manylinux builds 
 
 shopt -s expand_aliases
-alias cython="${PYBIN}/cython"
+echo "${BIN}"
+echo "${PYBIN}/cython"
+alias cython="${BIN}/cython"
 source ./update_cpp.sh
