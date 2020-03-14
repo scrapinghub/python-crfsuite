3d28312e5162a116d43b2ed901a24d006245f256
Mikhail Korobov <kmike84@gmail.com>
2020-03-15 01:32:17 +0500

    try enabling Python 3.8 on CI

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Scientific/Engineering",
