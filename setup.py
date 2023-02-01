1c114f262bacaf0a5164fb174e13e2812499fa53
Forest Gregg <fgregg@datamade.us>
2023-02-01 06:57:47 -0500

    v0.9.9 release

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ ext_modules = [Extension('pycrfsuite._pycrfsuite',
 
 setup(
     name='python-crfsuite',
-    version="0.9.8",
+    version="0.9.9",
     description="Python binding for CRFsuite",
     long_description=open('README.rst').read(),
     author="Terry Peng, Mikhail Korobov",
