d6af9213a17109aeebbe97d7eea6242b46e6939a
Forest Gregg <fgregg@datamade.us>
2024-09-27 14:16:03 -0400

    require py38

--- pycrfsuite/_pycrfsuite.pyx
+++ pycrfsuite/_pycrfsuite.pyx
@@ -3,7 +3,7 @@
 # cython: c_string_encoding=ascii
 # cython: profile=False
 # distutils: language=c++
-cimport pycrfsuite.crfsuite_api as crfsuite_api
+from . cimport crfsuite_api
 from libcpp.string cimport string
 
 import sys
