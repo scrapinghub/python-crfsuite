4db469a6bee76df4883eeddd90bbc6ec75eda7e0
Forest Gregg <fgregg@datamade.us>
2024-09-30 13:41:21 -0400

    build linux aarch64 wheels, closes #162

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ from distutils.command.build_ext import build_ext
 from Cython.Build import cythonize
 from setuptools import Extension, setup
 
-sources = ["pycrfsuite/_pycrfsuite.pyx",
-           "pycrfsuite/trainer_wrapper.cpp"]
+sources = ["pycrfsuite/_pycrfsuite.pyx", "pycrfsuite/trainer_wrapper.cpp"]
 
 # crfsuite
 sources += glob.glob("crfsuite/lib/crf/src/*.c")
@@ -34,12 +33,17 @@ class build_ext_check_gcc(build_ext):
         _compile = c._compile
 
         def c_compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
-            cc_args = cc_args + ["-D_POSIX_C_SOURCE=200112L"] if src.startswith("crfsuite/") else cc_args
+            cc_args = (
+                cc_args + ["-D_POSIX_C_SOURCE=200112L"]
+                if src.startswith("crfsuite/")
+                else cc_args
+            )
             cc_args = cc_args + ["-std=c99"] if src.endswith(".c") else cc_args
             return _compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
 
-        if c.compiler_type == "unix" and \
-           any(item == "gcc" or item.endswith("-gcc") for item in c.compiler):
+        if c.compiler_type == "unix" and any(
+            item == "gcc" or item.endswith("-gcc") for item in c.compiler
+        ):
             c._compile = c_compile
 
         elif self.compiler.compiler_type == "msvc":
@@ -60,5 +64,5 @@ setup(
             )
         ]
     ),
-    cmdclass={"build_ext": build_ext_check_gcc}
+    cmdclass={"build_ext": build_ext_check_gcc},
 )
